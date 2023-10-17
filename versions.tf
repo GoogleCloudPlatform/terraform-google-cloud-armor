5b0aa2ba243a02f4a6bf7819cb643db7b4818b9a
Imran Nayer <imrannayer@google.com>
2023-10-17 09:33:40 -0500

    fix: upgraded versions.tf to include minor bumps from tpg v5

--- versions.tf
+++ versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.79.0, < 5.0"
+      version = ">= 4.79.0, < 6.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.79.0, < 5.0"
+      version = ">= 4.79.0, < 6.0"
     }
   }
   provider_meta "google" {
